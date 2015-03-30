48cecbb01d5aff085c6b259fe413f15ca709ef0c
Strikingwolf <Strikingwolf2012@gmail.com>
2015-03-30 18:14:31 -0500

    Add producer names

--- main.rb
+++ main.rb
@@ -2,13 +2,17 @@ require_relative 'lib/elements'
 require_relative 'lib/producers'
 
 manager = ElementsManager.new
-ProducerManager.new 'air', 10, 1, manager
-ProducerManager.new 'earth', 5, 1, manager
+ProducerManager.new 'air', 10, 'Air Producer', 1, manager
+ProducerManager.new 'earth', 5, 'Earth Producer', 1, manager
 
 loop do # Main loop
   manager.tick
   sleep 1
   manager.get_elements.each do |key|
     p "We have #{manager.get_element key} elements of #{key}"
+  end
+
+  manager.producers.each do |producer|
+    p "We have #{producer.count} #{producer.name}s"
   end
 end
