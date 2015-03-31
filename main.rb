48b0d4d0b390a092852a1dd88f5db34ba6428337
Strikingwolf <Strikingwolf2012@gmail.com>
2015-03-31 16:44:18 -0500

    Replace with log_vals because conciseness

--- main.rb
+++ main.rb
@@ -8,11 +8,5 @@ ProducerManager.new 'earth', 5, 'Earth Producer', 1, manager
 loop do # Main loop
   manager.tick
   sleep 1
-  manager.get_elements.each do |key|
-    p "We have #{manager.get_element key} elements of #{key}"
-  end
-
-  manager.producers.each do |key, producer|
-    p "We have #{producer.count} #{key}s"
-  end
+  manager.log_vals
 end
