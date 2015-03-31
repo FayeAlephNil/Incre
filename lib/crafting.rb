5500fdc5151c80254318f00e8e7243654e3a1282
Strikingwolf <Strikingwolf2012@gmail.com>
2015-03-31 17:41:08 -0500

    Make crafting use things *facepalms*

--- lib/crafting.rb
+++ lib/crafting.rb
@@ -13,12 +13,27 @@ class CraftingManager
   def craft(ingredients, first = false)
     product = @recipes[ingredients]
     product.count = 1
-    if product != nil
-      if first_element
-        @manager.sub_managers[product.name] = product
-      else
-        @manager.produce product.name, 1
+    if checker(ingredients)
+      if product != nil
+        if first_element
+          @manager.sub_managers[product.name] = product
+        else
+          @manager.produce product.name, 1
+        end
+      end
+
+      ingredients.each do |key, amount|
+        @manager.use key, amount
       end
     end
+  end
+
+  def checker(ingredients)
+    ingredients.each do |key, count|
+      if @manager.sub_managers[key] = nil || !@manager.sub_managers[key].count < count
+        return false
+      end
+    end
+    return true
   end
 end
