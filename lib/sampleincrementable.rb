d3d3def100ef474ac4c770dbac15814de9c075cd
Strikingwolf <Strikingwolf2012@gmail.com>
2015-03-30 19:44:02 -0500

    DELETE THAT SAMPLEINCREMENTABLE

--- lib/sampleincrementable.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'baseincrementable'
-class SampleIncrementable < BaseIncrementable
-  @@name = :sample_incrementable
-  @@amount_per_increment = 1
-  @@linked_attr = :sample_count
-end
