cd75cd698df78b26aca7fc52307e7bb1d6b54b1f
Strikingwolf <Strikingwolf2012@gmail.com>
2015-04-02 22:00:39 -0500

    Add defaults to Hash.to_eval

--- lib/util/ruby_changes.rb
+++ lib/util/ruby_changes.rb
@@ -10,6 +10,8 @@ class Hash
     each do |key, producer|
       result << "#{key.to_eval} => #{producer.to_eval}"
     end
-    return "{#{result.join ", "}}"
+    hash = "{#{result.join ", "}}"
+
+    return "Hash.new(#{self.default}).merge(#{hash})"
   end
 end
