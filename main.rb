293288724e4bd32d3cf72beca0defcf689a17468
Strikingwolf <Strikingwolf2012@gmail.com>
2015-04-03 15:55:52 -0500

    Small Multithreading jack

--- main.rb
+++ main.rb
@@ -11,7 +11,11 @@ ProducerManager.new('earth', 5, 'Earth Producer', manager, 1)
 manager.crafter.add_recipe!({'Air Producer' => 1, 'Earth Producer' => 1}, ProducerManager.new('fire', 15, 'Fire Producer'))
 
 loop do # Main loop
-  manager.tick!
+  clone = manager.clone
+  threads = []
+  threads << Thread.new { clone.log_vals }
+  threads << Thread.new { manager = clone.tick }
+  threads.each { |thr| thr.join }
+
   sleep 1
-  manager.log_vals
 end
