d66616714c1e4d928f5261e0247a5e525a44b5c3
SkeletonPunk <skelepunk67@gmail.com>
2015-04-04 12:09:20 -0700

    more codes
    codes are moar

--- main.rb
+++ main.rb
@@ -6,6 +6,19 @@ manager = GameManager.new
 ProducerManager.new({'air' => 5, 'earth' => 4, 'water' => 3, 'fire' => 2}, 'Primal Core', manager, 1)
 manager.crafter.add_recipe!({'water' => 2, 'earth' => 5}, ElementManager.new 'mud')
 manager.crafter.add_recipe!({'air' => 3, 'fire' => 1}, ElementManager.new 'heat')
+manager.crafter.add_recipe!({'earth' => 20, 'fire' => 40}, ElementManager.new 'lava')
+manager.crafter.add_recipe!({'lava' => 20, 'water' => 10}, ElementManager.new 'obsidian')
+manager.crafter.add_recipe!({'heat' => 100, 'fire' => 20}, ElementManager.new 'energy')
+manager.crafter.add_recipe!({'water' => 20, 'fire' => 30}, ElementManager.new 'steam')
+manager.crafter.add_recipe!({'air' => 30, 'fire' => 20}, ElementManager.new 'wind')
+manager.crafter.add_recipe!({'lava' => 20, 'wind' => 10}, ElementManager.new 'stone')
+manager.crafter.add_recipe!({'stone' => 30, 'wind' => 50}, ElementManager.new 'gravel')
+manager.crafter.add_recipe!({'gravel' => 30, 'wind' => 50}, ElementManager.new 'sand')
+manager.crafter.add_recipe!({'mud' => 20, 'sand' => 20}, ElementManager.new 'clay')
+manager.crafter.add_recipe!({'sand' => 20, 'heat' => 70}, ElementManager.new 'glass')
+manager.crafter.add_recipe!({'gravel' => 20, 'heat' => 70}, ElementManager.new 'concrete')
+manager.crafter.add_recipe!({'clay' => 20, 'heat' => 70}, ElementManager.new 'brick')
+manager.crafter.add_recipe!({'mud' => 30, 'energy' => 100}, ElementManager.new 'organism')
 
 loop do # Main loop
   clone = manager.clone
